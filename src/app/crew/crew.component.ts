332cb22c39e2216649cc70eeb405e0e9c66ad209
Sumathi <thasel@gmail.com>
2021-04-18 02:34:26 -0500

    commited

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -1,29 +1,89 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-crew',
-  templateUrl: './crew.component.html',
-  styleUrls: ['./crew.component.css']
+  selector: "app-crew",
+  templateUrl: "./crew.component.html",
+  styleUrls: ["./crew.component.css"],
 })
 export class CrewComponent implements OnInit {
-
-  inCrew: boolean = false;
   crew: object[] = [];
+  currPhoto: string = "";
+  currID: number = null;
+  displayImage: boolean = false;
 
   candidates: object[] = [
-    {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
-    {name: "Mae Jemison", photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg'},
-    {name: "Ellen Ochoa", photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg'},
-    {name: "Frederick Gregory", photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg'},
-    {name: "Guion Bluford", photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg'},
-    {name: "Kjell Lindgren", photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg'},
-    {name: "Jeanette Epps", photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg'}
+    {
+      name: "Sally Ride",
+      photo:
+        "https://handlers.education.launchcode.org/static/images/sally-ride.jpg",
+      astronautID: 457,
+    },
+    {
+      name: "Mae Jemison",
+      photo:
+        "https://handlers.education.launchcode.org/static/images/mae-jemison.jpg",
+      astronautID: 276,
+    },
+    {
+      name: "Ellen Ochoa",
+      photo:
+        "https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg",
+      astronautID: 112,
+    },
+    {
+      name: "Frederick Gregory",
+      photo:
+        "https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg",
+      astronautID: 900,
+    },
+    {
+      name: "Guion Bluford",
+      photo:
+        "https://handlers.education.launchcode.org/static/images/guion-bluford.jpg",
+      astronautID: 388,
+    },
+    {
+      name: "Kjell Lindgren",
+      photo:
+        "https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg",
+      astronautID: 209,
+    },
+    {
+      name: "Jeanette Epps",
+      photo:
+        "https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg",
+      astronautID: 444,
+    },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   // Code the 'addCrewMember' function here:
+  addCrewMember(candidate: object): void {
+    const index = this.crew.indexOf(candidate);
+    if (index < 0 && this.crew.length < 3) {
+      candidate["inCrew"] = true;
+      this.crew.push(candidate);
+    } else if (index > -1) {
+      candidate["inCrew"] = false;
+      this.crew.splice(index, 1);
+    }
+  }
 
+  findImage(candidate: object): void {
+    const photo = candidate["photo"];
+    const id = candidate["astronautID"];
+    this.currPhoto = photo;
+    this.currID = id;
+  }
+
+  emptyCrew(): void {
+    for (let i = 0; i < this.crew.length; i++) {
+      const member = this.crew[i];
+      member["inCrew"] = false;
+    }
+    this.crew = [];
+  }
 }
