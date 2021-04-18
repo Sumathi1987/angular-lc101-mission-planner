c01f4ad222b74c825672b2e24e99fa4b8c7adcff
Sumathi <thasel@gmail.com>
2021-04-18 01:04:59 -0500

    commited 2

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -1,21 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-crew',
-  templateUrl: './crew.component.html',
-  styleUrls: ['./crew.component.css']
+  selector: "app-crew",
+  templateUrl: "./crew.component.html",
+  styleUrls: ["./crew.component.css"],
 })
 export class CrewComponent implements OnInit {
-
   crew: object[] = [
-    {name: "Eileen Collins", firstMission: false},
-    {name: "Mae Jemison", firstMission: false},
-    {name: "Ellen Ochoa", firstMission: true}
+    { name: "Eileen Collins", firstMission: false },
+    { name: "Mae Jemison", firstMission: false },
+    { name: "Ellen Ochoa", firstMission: true },
   ];
+  memberBeingEdited: object = null;
+  inCrew: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  add(memberName: string, isFirst: boolean) {
+    for (let i = 0; i < this.crew.length; i++) {
+      let crewMember = this.crew[i];
+      let name = crewMember["name"];
+      if (name === memberName || memberName === "") {
+        this.inCrew = true;
+      } else {
+        this.inCrew = false;
+      }
+    }
+    if (this.inCrew) {
+      alert("You must enter a new crew member");
+    } else {
+      this.crew.push({ name: memberName, firstMission: isFirst });
+    }
   }
 
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+
+  save(name: string, member: object) {
+    member["name"] = name;
+    this.memberBeingEdited = null;
+  }
 }
